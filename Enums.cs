a0ed3804b8c85ac75909904012a56a8ccbdbe819
SkataBg <vasilev04lybo@gmail.com>
2026-06-23 19:03:22 +0300

    Add files via upload

---
+++ Enums.cs
@@ -0,0 +1,17 @@
+﻿namespace TCSA.OOP.LibraryManagement;
+internal class Enums
+{
+    internal enum MenuAction
+    {
+        ViewItems,
+        AddItem,
+        DeleteItem
+    }
+
+    internal enum ItemType
+    {
+        Book,
+        Magazine,
+        Newspaper
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using Spectre.Console;
+//using TCSA.OOP.LibraryManagementSystem;
+using TCSA.OOP.LibraryManagement;
+
+UserInterface userInterface = new UserInterface();
+userInterface.MainMenu();
+
+//(string Title, int Pages) book1 = ("Frankestein", 250);
+//Console.WriteLine($"{book1.Title}, Pages: {book1.Pages}");
+
+//var book1 = new Book("Frankestein", 250);
+//var book2 = new Book("Great Gatsby", 300);
+//var book3 = new Book();
+
+
+//Animal myDog = new Dog("Buddy", 3);
+//Animal myCat = new Cat("Whiskers", 2);
+
+//myDog.MakeSound(); // Output: Buddy says: Woof!
+//myCat.MakeSound(); // Output: Whiskers says: Meow!
+//internal abstract class Animal
+//{
+//    public string Name { get; set; }
+//    public int Age { get; set; }
+
+//    protected Animal(string name, int age)
+//    {
+//        Name = name;
+//        Age = age;
+//    }
+
+//    // Abstract method that must be implemented by derived classes
+//    public abstract void MakeSound();
+//}
+
+//internal class Dog : Animal
+//{
+//    public Dog(string name, int age)
+//        : base(name, age)
+//    {
+//    }
+
+//    public override void MakeSound()
+//    {
+//        Console.WriteLine($"{Name} says: Woof!");
+//    }
+//}
+
+//internal class Cat : Animal
+//{
+//    public Cat(string name, int age)
+//        : base(name, age) // Calling the base class constructor
+//    {
+//    }
+
+//    public override void MakeSound()
+//    {
+//        Console.WriteLine($"{Name} says: Meow!");
+//    }
+//}
+
